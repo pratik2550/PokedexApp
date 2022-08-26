631d976d7a65d6fde0039ef6cc62aaac8df923e8
pratik2550 <pratikpatle250@gmail.com>
2022-08-26 22:44:41 +0530

    Added retrofit interface.

--- app/src/main/java/com/example/pokedexapp/daggerHilt/ApplicationModule.kt
+++ app/src/main/java/com/example/pokedexapp/daggerHilt/ApplicationModule.kt
@@ -1,11 +1,35 @@
 package com.example.pokedexapp.daggerHilt
 
+import com.example.pokedexapp.data.remote.PokeApi
+import com.example.pokedexapp.repository.PokemonRepository
+import com.example.pokedexapp.util.Constants.BASE_URL
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object ApplicationModule {
 
+    @Singleton
+    @Provides
+    fun providePokemonRepository(
+        api: PokeApi
+    ) = PokemonRepository(api)
+
+    @Singleton
+    @Provides
+    fun providePokeApi(): PokeApi {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+            .create(PokeApi::class.java)
+
+    }
 }
